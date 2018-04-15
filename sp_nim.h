cc0ee330b65506000fb730958958e15cc7170e91
Jonatan Hadas <jonatanhadas@localhost.localdomain>
2018-04-15 14:45:29 +0300

    Write interface in sp_nim.h

---
+++ sp_nim.h
@@ -0,0 +1,25 @@
+/*
+maximum number of haeps
+*/
+#define MAX_HEAPS
+
+/*
+returns whether all heaps are empty (last player wins)
+receives game configuration: number of heaps and array of heap heights.
+*/
+int check_win(int heap_num, int heaps[MAX_HEAPS]);
+
+/*
+implements a move
+receives game configuration, heap index and amount taken,
+if move is illeagal, returns 1 and does nothing.
+if move is leagal, returns 0 and changes heap, accordingly
+*/
+int move(int heap_num, int heaps[MAX_HEAPS], int heap_index, int amount);
+
+/*
+recieves configuration
+also receives array of 2 ints,
+this array will be changed to {heap_index, amount}: the computers moves
+*/
+void get_computer_move(int heap_num, int heaps[MAX_HEAPS], int out[2]);
