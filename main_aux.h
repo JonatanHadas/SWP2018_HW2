23bcb9bd0c80fbcbcabab366c111b606fa8d2ac6
Eduard-Gorohovski <37761050+Eduard-Gorohovski@users.noreply.github.com>
2018-04-15 15:33:02 +0300

    Update main_aux.h

---
+++ main_aux.h
@@ -0,0 +1,17 @@
+/*
+ * main_aux.h
+ *
+ *  Created on: Apr 15, 2018
+ *      Author: User1
+ */
+
+#ifndef MAIN_AUX_H_
+#define MAIN_AUX_H_
+
+
+int inputHeapSizes(int* sizes, int numHeaps);
+
+int numHeaps(int maxNumHeaps);
+
+
+#endif /* MAIN_AUX_H_ */
