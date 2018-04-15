de53669c28b2f12079ae94af3bd7b9d24d1a7b3b
Eduard-Gorohovski <37761050+Eduard-Gorohovski@users.noreply.github.com>
2018-04-15 15:34:51 +0300

    Update main_aux.c

--- main_aux.c
+++ main_aux.c
@@ -15,7 +15,7 @@ int inputHeapSizes(int* sizes, int numHeaps) {
 	int scanCheck=0, i=0;
 	printf("Enter the heap sizes:\n");
 	for(i=0;i<numHeaps;i++){
-		scanCheck=scanf("%d", *(sizes+i));
+		scanCheck=scanf("%d", (sizes+i));
 		if(scanCheck==0 || sizes[i]<=0){
 			printf("Error: the size of heap %d should be positive.\n", i+1);
 			return 0;
@@ -27,7 +27,7 @@ int inputHeapSizes(int* sizes, int numHeaps) {
 int numHeaps(int maxNumHeaps){
 	int scanCheck=0, numHeaps=0;
 	printf("Enter the number of heaps:\n");
-	scanCheck=scanf("%d", numHeaps);
+	scanCheck=scanf("%d", &numHeaps);
 	if(scanCheck==0 || numHeaps<1 || numHeaps>maxNumHeaps){
 		printf("Error: the number of heaps must be between 1 and 32.\n");
 		return 0;
