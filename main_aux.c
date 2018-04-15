754733a08d426c271a56a1dbf09fe036e263fd74
Eduard-Gorohovski <37761050+Eduard-Gorohovski@users.noreply.github.com>
2018-04-15 15:32:38 +0300

    Update main_aux.c

---
+++ main_aux.c
@@ -0,0 +1,36 @@
+/*
+ ============================================================================
+ Name        : SP2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int inputHeapSizes(int* sizes, int numHeaps) {
+	int scanCheck=0, i=0;
+	printf("Enter the heap sizes:\n");
+	for(i=0;i<numHeaps;i++){
+		scanCheck=scanf("%d", *(sizes+i));
+		if(scanCheck==0 || sizes[i]<=0){
+			printf("Error: the size of heap %d should be positive.\n", i+1);
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int numHeaps(int maxNumHeaps){
+	int scanCheck=0, numHeaps=0;
+	printf("Enter the number of heaps:\n");
+	scanCheck=scanf("%d", numHeaps);
+	if(scanCheck==0 || numHeaps<1 || numHeaps>maxNumHeaps){
+		printf("Error: the number of heaps must be between 1 and 32.\n");
+		return 0;
+	}
+	return numHeaps;
+}
