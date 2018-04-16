54d4a9db860dc3d79d59b635ce0a3b18fc89c47a
JonatanHadas <github@jonatan.hadaso.net>
2018-04-16 22:46:47 +0300

    Add comments

--- main.c
+++ main.c
@@ -4,31 +4,43 @@
 #include "sp_nim.h"
 
 int main(void) {
+	/* number of heaps,  , turn number */
 	int heap_num, check=0, turn=0;
+	/* heap sizes as array */
 	int heapsSizes[MAX_HEAPS];
+	/* used to store move (pair of numbers) */
 	int turnMove[2];
+	
+	/* initialize game */
 	heap_num=numHeaps(MAX_HEAPS);
 	if(heap_num==0) return 0;
 	check=inputHeapSizes(heapsSizes, heap_num);
 	if(check==0) return 0;
+	
+	/* continue as long as game is not finished */
 	while(check_win(heap_num, heapsSizes)==0){
 		turn++;
 		printStatus(heap_num, heapsSizes, turn);
+		
+		/* odd turns are player turns */
 		if(turn%2==1){
 			printf("Your turn: please enter the heap index and the number of removed objects.\n");
 			getMove(turnMove);
+			/* if entered move is illegal loop until legal move is given */
 			while(move(heap_num, heapsSizes, turnMove[0], turnMove[1])==1){
 				printf("Error: Invalid input.\nPlease enter again the heap index and the number of removed objects.\n");
 				getMove(turnMove);
 			}
 			printf("You take %d objects from heap %d.\n",turnMove[1],turnMove[0]+1);
 		}
+		/* even turns are computer turns */
 		else{
 			get_computer_move(heap_num, heapsSizes,turnMove);
 			move(heap_num, heapsSizes, turnMove[0], turnMove[1]);
 			printf("Computer takes %d objects from heap %d.\n",turnMove[1],turnMove[0]+1);
 		}
 	}
+	/* parity of last turn indicates the winner */
 	if(turn%2==1){
 		printf("You win!\n");
 	}
--- main_aux.h
+++ main_aux.h
@@ -8,13 +8,44 @@
 #ifndef MAIN_AUX_H_
 #define MAIN_AUX_H_
 
+/*
+gets heap sizes from standard input.
+writes sizes into "int* sizes".
+writes "int numHeaps" heaps.
 
+returns 1 on success and 0 on failure.
+*/
 int inputHeapSizes(int* sizes, int numHeaps);
 
+/*
+gets heap number from standard input
+and returns it.
+"int maxNumHeaps" is maximum allowed number of heaps.
+
+if fails prints error message and returns 0.
+*/
 int numHeaps(int maxNumHeaps);
 
+/*
+prints status:
+turn number and heap sizes.
+
+receives as parameters:
+numHeaps: number of heaps
+heaps: heap sizes in array
+turn: number of turn
+*/
 void printStatus(int numHeaps, int* heaps, int turn);
 
+/*
+gets move from standard input
+and writes it into "int out[2]".
+
+first number describes heap index
+second number describes amount
+
+on failure writes {-2,-1}  (illegal move)
+*/
 void getMove(int move[2]);
 
 
